debe83ff55dda0c5ef85685aeb101d9cbd5c1256
Sujith <syjithkpy@gmail.com>
2026-03-08 05:14:10 +0000

    * solved search  issue

--- app/index.tsx
+++ app/index.tsx
@@ -1,148 +1,145 @@
-import Ionicons from "@expo/vector-icons/Ionicons"
-import { Image } from "expo-image"
-import { router, Stack, useNavigation } from "expo-router"
-import { useEffect, useState } from "react"
-import { ActivityIndicator, FlatList, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from "react-native"
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Image } from "expo-image";
+import { router, Stack, useNavigation } from "expo-router";
+import { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
-interface Pokemon{
-    name:string,
-    url:string ,
-    imageBack:string,
-    types:PokemoneTypes[]
+interface Pokemon {
+  name: string;
+  url: string;
+  imageBack: string;
+  types: PokemoneTypes[];
 }
-interface PokemoneTypes{
-    type:{
-        name:string,
-        url:string
-    }
-}
-
-const ColorType={
-    grass:'#78c850',
-    fire:'#f08030',
-    water:'#6890f0',
-    bug:'#a8b820',
-    normal:'#a8a878',
-    poison:'#a040a0',
-    electric:'#f8d030',
-    ground:'#e0c068',
-    fairy:'#ee99ac',
-    fighting:'#c03028',
-    psychic:'#f85888',
-    rock:'#b8a038',
-    ghost:'#705898',
-    ice:'#98d8d8',
-    dragon:'#7038f8',
-    dark:'#705848',
-    steel:'#b8b8d0',
-    flying:'#a890f0'
+interface PokemoneTypes {
+  type: {
+    name: string;
+    url: string;
+  };
 }
 
-const LIMIT=5
+const ColorType = {
+  grass: "#78c850",
+  fire: "#f08030",
+  water: "#6890f0",
+  bug: "#a8b820",
+  normal: "#a8a878",
+  poison: "#a040a0",
+  electric: "#f8d030",
+  ground: "#e0c068",
+  fairy: "#ee99ac",
+  fighting: "#c03028",
+  psychic: "#f85888",
+  rock: "#b8a038",
+  ghost: "#705898",
+  ice: "#98d8d8",
+  dragon: "#7038f8",
+  dark: "#705848",
+  steel: "#b8b8d0",
+  flying: "#a890f0",
+};
+
+const LIMIT = 5;
 const pokemon = () => {
-      const [data,setData]=useState<Pokemon[]>([])
-      const navigate=useNavigation()
+  const [data, setData] = useState<Pokemon[]>([]);
+  const navigate = useNavigation();
   const [showSearch, setShowSearch] = useState(false);
   const [query, setQuery] = useState("");
-  const [page,setPage]=useState(0)
-const [loading,setLoading]=useState(false)
+  const [page, setPage] = useState(0);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(()=>{
-if(!query)fetchdata(page)
-  }
-  ,[page])
+  useEffect(() => {
+    if (!query) fetchdata(page);
+  }, [page]);
 
-  const  fetchdata=async(page:number)=>{
+  const fetchdata = async (page: number) => {
     try {
-        
-        setLoading(true)
-  
-    const offset= page*LIMIT
-    const res=await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}&offset=${offset}`)
-    
-    const data=await res.json()
+      setLoading(true);
 
-    const detailsPokiemon=await Promise.all(
+      const offset = page * LIMIT;
+      const res = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}&offset=${offset}`,
+      );
 
-        data.results.map(async(pokemon:{name:string,url:string})=>{
-            const details=await fetch(pokemon.url)
-            const detailsData=await details.json()
-            return{
-                name:detailsData.name,
-                url:detailsData.sprites.front_default,
-                imageBack:detailsData.sprites.back_default,
-                types:detailsData.types
-            }
-           
-        }
+      const data = await res.json();
 
-
-
-    )
-
-    )
+      const detailsPokiemon = await Promise.all(
+        data.results.map(async (pokemon: { name: string; url: string }) => {
+          const details = await fetch(pokemon.url);
+          const detailsData = await details.json();
+          return {
+            name: detailsData.name,
+            url: detailsData.sprites.front_default,
+            imageBack: detailsData.sprites.back_default,
+            types: detailsData.types,
+          };
+        }),
+      );
 
       setData((prev) => [...prev, ...detailsPokiemon]);
-
     } catch (error) {
-        console.log(error)
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
-    finally{
-       setLoading(false)
-    }
-  }
+  };
 
-
-  const searchPokemons=async(name:string)=>{
-    if(!name)return
+  const searchPokemons = async (name: string) => {
+    if (!name) return;
     try {
-        setLoading(true)
-        const searchquery=name.trim().toLowerCase()
-    const res=await fetch(`https://pokeapi.co/api/v2/pokemon/${searchquery}`)
-    console.log('res',res)
-    const data=await res.json()
+      setLoading(true);
+      const searchquery = name.trim().toLowerCase();
+      const res = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${searchquery}`,
+      );
+      console.log("res", res);
+      const data = await res.json();
 
-console.log('dataa',data)
+      console.log("dataa", data);
 
-    const detailsPokiemon={
-        name:data.name,
-        url:data.sprites.front_default,
-        imageBack:data.sprites.back_default,
-        types:data.types
-    }
-    setData([detailsPokiemon])
-    
+      const detailsPokiemon = {
+        name: data.name,
+        url: data.sprites.front_default,
+        imageBack: data.sprites.back_default,
+        types: data.types,
+      };
+      setData([detailsPokiemon]);
     } catch (error) {
-        console.log(error)
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
-    finally{
-        setLoading(false)
-    }
-  }
+  };
 
-  
-  useEffect(()=>{
-    if(query){
-        console.log('query',query)
-        searchPokemons(query)
+  useEffect(() => {
+    if (query) {
+      console.log("query", query);
+      searchPokemons(query);
+    } else {
+      setData([]);
+      setPage(0);
+      fetchdata(0);
     }
-    else{
-        setData([])
-        setPage(0)
-        fetchdata(0)
-    }
-  },[query])
+  }, [query]);
   return (
-   <>
-       <Stack.Screen
+    <>
+      <Stack.Screen
         options={{
-             headerTitle: () =>
+          headerTitle: () =>
             showSearch ? (
               <TextInput
                 placeholder="Search Pokémon..."
                 value={query}
                 onChangeText={(text) => {
-                    console.log(text)
+                  console.log(text);
                   setQuery(text);
                 }}
                 autoFocus
@@ -154,10 +151,15 @@ console.log('dataa',data)
                 }}
               />
             ) : (
-              <Text style={{ fontWeight: "bold"  ,color:'#fff', fontSize: 20}}>Pokémon</Text>
+              <Text style={{ fontWeight: "bold", color: "#fff", fontSize: 20 }}>
+                Pokémon
+              </Text>
             ),
           headerRight: () => (
-             <Pressable onPress={() => setShowSearch(!showSearch)}>
+            <Pressable onPress={() => {
+              setShowSearch(!showSearch)
+              setQuery('')
+            }}>
               <Ionicons
                 name={showSearch ? "close" : "search"}
                 size={24}
@@ -165,55 +167,55 @@ console.log('dataa',data)
               />
             </Pressable>
           ),
-          
         }}
       />
 
-      <FlatList data={data} keyExtractor={(item,index)=>index.toString()}
-       renderItem={({ item ,index}) => (
-     
-   <ScrollView 
-contentContainerStyle={{
+      <FlatList
+        data={data}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item, index }) => (
+          <ScrollView
+            contentContainerStyle={{
+              gap: 16,
+              padding: 16,
+            }}
+          >
+            <Pressable
+              key={index}
+              onPress={() =>
+                router.push({
+                  pathname: "/details",
+                  params: { name: item.name },
+                })
+              }
+              style={{
+                backgroundColor:
+                  ColorType[item.types[0].type.name as keyof typeof ColorType] +
+                  "33",
+                padding: 20,
+                borderRadius: 20,
+              }}
+            >
+              <View>
+                {/* <Text style={sytles.indexText}>{index+1}</Text> */}
+                <Text style={sytles.name}>{item.name}</Text>
+                <Text style={sytles.type}>{item.types[0].type.name}</Text>
+                <View style={{ flexDirection: "row" }}>
+                  <Image
+                    source={{ uri: item.url }}
+                    style={sytles.imagecontainer}
+                  />
 
-
-    gap:16,
-    padding:16
-}}   
-   >
-          <Pressable key={index} 
-  onPress={() => router.push({pathname:'/details',params:{name:item.name}})}
-        style={
-            {
-                backgroundColor:ColorType[item.types[0].type.name as keyof typeof ColorType]+'33',
-                padding:20,
-                borderRadius:20,
-            }
-        }
-        >
-
-        <View  >
-            {/* <Text style={sytles.indexText}>{index+1}</Text> */}
-            <Text style={sytles.name}>{item.name}</Text>
-            <Text style={sytles.type}>{item.types[0].type.name}</Text>
-            <View style={{flexDirection:'row'}}>
-
-
-            <Image   source={{uri:item.url}} style={sytles.imagecontainer}/>
-
-            <Image   source={{uri:item.imageBack}} style={sytles.imagecontainer}/>
-
-            </View>
-
-        </View>
-                </Pressable>
-
-
-                   </ScrollView>
-
-
-
+                  <Image
+                    source={{ uri: item.imageBack }}
+                    style={sytles.imagecontainer}
+                  />
+                </View>
+              </View>
+            </Pressable>
+          </ScrollView>
         )}
-          onEndReached={() => {
+        onEndReached={() => {
           if (!query) setPage((p) => p + 1);
         }}
         onEndReachedThreshold={0.5}
@@ -221,44 +223,38 @@ contentContainerStyle={{
           loading ? <ActivityIndicator style={{ margin: 20 }} /> : null
         }
       />
+    </>
+  );
+};
 
-   
-   </>
-  )
-}
+export default pokemon;
 
-export default pokemon
+const sytles = StyleSheet.create({
+  name: {
+    fontSize: 28,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  type: {
+    fontSize: 21,
+    fontWeight: "bold",
+    color: "gray",
+    textAlign: "center",
+  },
 
+  //      container: {
 
+  //         backgroundColor: 'red',
 
-const sytles=StyleSheet.create({
-    name:{
-        fontSize:28,
-        fontWeight:'bold',
-        textAlign:'center'
-    },
-      type:{
-        fontSize:21,
-        fontWeight:'bold',
-        color:'gray',
-        textAlign:'center'
-
-    },
-
-//      container: {
-     
-//         backgroundColor: 'red',
-       
-//   },
+  //   },
   text: {
-    color: '#0000',
+    color: "#0000",
   },
-  indexText:{
-    
-    color:'red'
+  indexText: {
+    color: "red",
   },
-  imagecontainer:{
-    width:100,
-    height:100
-  }
-})
+  imagecontainer: {
+    width: 100,
+    height: 100,
+  },
+});
